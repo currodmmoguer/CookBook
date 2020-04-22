926f0b5b47ab3a2968319a3b6da54d0e15a602b7
currodmmoguer <currodmmoguer@gmail.com>
2020-04-22 09:37:13 +0200

    Para Heroku

--- cookbook/wsgi.py
+++ cookbook/wsgi.py
@@ -15,5 +15,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookbook.settings')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
