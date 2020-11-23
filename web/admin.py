532c7080fdda409aea1080b66edafdc8cb1acb5f
currodmmoguer <currodmmoguer@gmail.com>
2020-11-23 20:49:33 +0100

    Base de datos vacia

--- web/admin.py
+++ web/admin.py
@@ -9,38 +9,38 @@ from django.core.serializers.json import DjangoJSONEncoder
 import json
 
 
-@admin.register(Categoria)
-class CategoriaReport(admin.ModelAdmin):
-    list_display = ('nombre',)  # El dato del objeto que se ve en la lista
-    ordering = ('nombre',)  #Orden de la lista, ordenado alfabéticamente
-    categorias = Receta.objects.values('categoria').annotate(total=Count('id')) #Obtiene una lista con el id de la categoría y su cantidad de recetas
+# @admin.register(Categoria)
+# class CategoriaReport(admin.ModelAdmin):
+#     list_display = ('nombre',)  # El dato del objeto que se ve en la lista
+#     ordering = ('nombre',)  #Orden de la lista, ordenado alfabéticamente
+#     categorias = Receta.objects.values('categoria').annotate(total=Count('id')) #Obtiene una lista con el id de la categoría y su cantidad de recetas
     
-    for categoria in categorias: #Añade a cada item de la lista el nombre de la categoría
-        categoria['categoria'] = Categoria.objects.get(pk=categoria['categoria']).nombre
+#     for categoria in categorias: #Añade a cada item de la lista el nombre de la categoría
+#         categoria['categoria'] = Categoria.objects.get(pk=categoria['categoria']).nombre
     
-    def changelist_view(self, request, extra_context=None): # Envía los datos a la template
-        as_json = json.dumps(list(self.categorias), cls=DjangoJSONEncoder)   # Convierte la lista en un json
-        extra_context = {"chart_data": as_json}
-        return super().changelist_view(request, extra_context=extra_context)
+#     def changelist_view(self, request, extra_context=None): # Envía los datos a la template
+#         as_json = json.dumps(list(self.categorias), cls=DjangoJSONEncoder)   # Convierte la lista en un json
+#         extra_context = {"chart_data": as_json}
+#         return super().changelist_view(request, extra_context=extra_context)
 
-@admin.register(Perfil)
-class Perfil(admin.ModelAdmin):
-    exclude = ('seguidores',)   # Elimina el campo seguidores del administrador
+# @admin.register(Perfil)
+# class Perfil(admin.ModelAdmin):
+#     exclude = ('seguidores',)   # Elimina el campo seguidores del administrador
 
-    # Obtiene los 10 perfiles con más recetas creadas con su cantidad de recetas
-    perfiles = User.objects.filter(perfil__isnull=False).annotate(num_recetas=Count('recetas')).filter(num_recetas__gt=0).order_by('-num_recetas')[:5]
-    lista = []
+#     # Obtiene los 10 perfiles con más recetas creadas con su cantidad de recetas
+#     perfiles = User.objects.filter(perfil__isnull=False).annotate(num_recetas=Count('recetas')).filter(num_recetas__gt=0).order_by('-num_recetas')[:5]
+#     lista = []
     
-    for perfil in perfiles: # Añade los perfiles con la cantidad de recetas a lista de diccionarios
-        dict = {}
-        dict['username'] = perfil.username
-        dict['total_recetas'] = perfil.num_recetas
-        lista.append(dict)
+#     for perfil in perfiles: # Añade los perfiles con la cantidad de recetas a lista de diccionarios
+#         dict = {}
+#         dict['username'] = perfil.username
+#         dict['total_recetas'] = perfil.num_recetas
+#         lista.append(dict)
     
-    def changelist_view(self, request, extra_context=None): # Envía los datos al template
-        as_json = json.dumps(list(self.lista), cls=DjangoJSONEncoder)   # Convierte la lista en un json
-        extra_context = {"chart_data": as_json}
-        return super().changelist_view(request, extra_context=extra_context)
+#     def changelist_view(self, request, extra_context=None): # Envía los datos al template
+#         as_json = json.dumps(list(self.lista), cls=DjangoJSONEncoder)   # Convierte la lista en un json
+#         extra_context = {"chart_data": as_json}
+#         return super().changelist_view(request, extra_context=extra_context)
 
 
 @admin.register(Sugerencia)
